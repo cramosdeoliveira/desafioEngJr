ef12b8d2dfa46e9e73c12201604160555ba7881b
cristiano <cristianoliveira.ramos@gmail.com>
2021-06-17 14:29:52 -0300

    service novo usuario criado

--- api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.http.post('http://localhost:3000/user/signup', novoUsuario)
   }
 
 }
