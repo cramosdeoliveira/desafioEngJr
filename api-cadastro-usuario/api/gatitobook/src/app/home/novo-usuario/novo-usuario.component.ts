c7681d062bab14ff9a006333a4fb7188e76e9e05
cristiano <cristianoliveira.ramos@gmail.com>
2021-06-17 14:53:55 -0300

    corrigindo a rota para o service

--- api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,13 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.http.post('http://localhost:3000/user/signup', novoUsuario)
-  }
+  
 
 }
