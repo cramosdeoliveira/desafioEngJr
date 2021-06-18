96baadf02bd814ae3f3ec7f298f4d82bba9caa21
cristiano <cristianoliveira.ramos@gmail.com>
2021-06-17 23:05:42 -0300

    problemas nos pacotes e erro no pipe

--- api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -15,7 +16,8 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService
   ) { }
 
   ngOnInit(): void {
@@ -29,7 +31,7 @@ export class NovoUsuarioComponent implements OnInit {
         Validators.required,
         Validators.minLength(4)
       ]],
-      userName: ['', [minusculoValidator]],
+      userName: ['', [minusculoValidator], [this.usuarioExisteService.usuarioJaExiste()]],
       password: ['']
     })
   }
--- api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -13,4 +13,7 @@ export class NovoUsuarioService {
     return this.http.post('http://localhost:3000/user/signup', novoUsuario)
   }
   
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`)
+  }
 }
