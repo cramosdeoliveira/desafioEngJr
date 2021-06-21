ec810a8ececfb5e943effa2f05842392c95eae2b
cristiano <cristianoliveira.ramos@gmail.com>
2021-06-20 21:59:14 -0300

    metodos curtir e excluir fotos criados e implementados, component comentarios, service comentarios e interface comentarios criados

--- api-cadastro-usuario/api/gatitobook/src/app/animais/animais.service.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
 import { Animais, Animal } from './animais';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL
+const NOT_MODIFIED = '304'
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +27,18 @@ export class AnimaisService {
     return this.http.get<Animal>(`${API}/photos/${id}`)
   }
 
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`)
+  }
+
+  curtir(id: number): Observable<Boolean> {
+    return this.http.post(`${API}/photos/${id}/like` ,
+      {},
+      {observe: 'response'})
+      .pipe(
+        mapTo(true), catchError((error) => {
+          return error.status == NOT_MODIFIED ? of(false) : throwError(error)
+        })
+      )
+  }
 }
