e196d3cc748ba2d2438ba4f2297237334ab5b48e
cristiano <cristianoliveira.ramos@gmail.com>
2021-06-20 22:37:00 -0300

    metodo gravar implementado no component comentarios

--- api-cadastro-usuario/api/gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ api-cadastro-usuario/api/gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,5 +1,8 @@
+import { switchMap, tap } from 'rxjs/operators';
+import { ComentariosService } from './comentarios.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Comentarios } from './comentarios';
 
 @Component({
@@ -8,11 +11,32 @@ import { Comentarios } from './comentarios';
   styleUrls: ['./comentarios.component.css']
 })
 export class ComentariosComponent implements OnInit {
+  @Input() id!: number
   comentarios$!: Observable<Comentarios>
+  comentarioForm!: FormGroup
 
-  constructor() { }
+  constructor(
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.comentarios$ = this.comentariosService.buscaComentario(this.id)
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)]
+    })
   }
 
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? ''
+    this.comentarios$ = this.comentariosService.incluiComentario(
+      this.id,
+      comentario
+    ).pipe(switchMap(() => this.comentariosService.buscaComentario(this.id)),
+    tap(() => {
+      this.comentarioForm.reset()
+      alert('Comentário Salvo!!!')
+    })
+    )
+  }
 }
